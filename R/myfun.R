f1b5478f21ff36a0308993c8b1230b98bc856904
barbiniC47 <Emily.Barbini@ou.edu>
2022-07-22 17:46:10 -0500

    Add mybin

---
+++ R/myfun.R
@@ -0,0 +1,13 @@
+#' @title Square a vector
+#'
+#' @param x A vector of quantitative values
+#'
+#' @return A vector of squared values
+#' @export
+#'
+#' @examples myfun(x = 1:4)
+#'
+myfun <- function(x){
+  x^2
+}
+
---
+++ R/myncurve.R
@@ -0,0 +1,5 @@
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+  return(myncurve)
+
+}
