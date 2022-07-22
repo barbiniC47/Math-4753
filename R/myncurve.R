c9e3184bfaf1e558d3a7668d708fa59452a2517a
barbiniC47 <Emily.Barbini@ou.edu>
2022-07-22 18:35:53 -0500

    Add myfun

--- R/myncurve.R
+++ R/myncurve.R
@@ -1,5 +1,11 @@
-myncurve = function(mu, sigma, a){
-  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+myncurve <= function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim=c(mu-3*sigma, mu + 3*sigma), col = "black")
+
+  xcurve = seq(-Inf, a, length = 1000)
+  ycurve = dnorm(xcurve, mu, sigma)
+  polygon(c(-inf, xcurve, a), c(0, ycurve, 0), col = "blue")
+
+  prob = round(pnorm(a, mu, sigma), 4)
   return(myncurve)
 
 }
