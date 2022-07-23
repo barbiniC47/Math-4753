165fd509e06931e860d722f901783014ee6edc48
barbiniC47 <Emily.Barbini@ou.edu>
2022-07-22 19:54:30 -0500

    Add myncurve

--- R/myncurve.R
+++ R/myncurve.R
@@ -1,4 +1,11 @@
-
+#' Function to make a curve and graph the area underneath it. For probabilities with a normal distribution.
+#'
+#'
+#'
+#'
+#'
+#'
+#'
 myncurve <= function(mu, sigma, a){
   curve(dnorm(x,mean=mu,sd=sigma), xlim=c(mu-3*sigma, mu + 3*sigma), col = "black")
 
